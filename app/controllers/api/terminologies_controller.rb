42ec9da8f0c290018d23f9309dec50dc5ca29d51
Yitzy Bratt <yitzybratt32@gmail.com>
2021-03-17 16:15:39 -0400

    adds dynamic search

--- app/controllers/api/terminologies_controller.rb
+++ app/controllers/api/terminologies_controller.rb
@@ -1,6 +1,6 @@
 class Api::TerminologiesController < ApplicationController
   def show
-    response = HTTP.get("https://dictionaryapi.com/api/v3/references/thesaurus/json/polka?key=#{Rails.application.credentials.terminologies_api[:api_key]}")
+    response = HTTP.get("https://dictionaryapi.com/api/v3/references/thesaurus/json/#{params[:search]}?key=#{Rails.application.credentials.terminologies_api[:api_key]}")
     render json: response.parse
   end
 end
